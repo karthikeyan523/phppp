315d6d4211b4cb357724a0a318408d633f398180
Karthi Ninja <74287757+karthikeyan523@users.noreply.github.com>
2022-07-12 17:29:36 +0530

    Update phpp.sh

--- phpp.sh
+++ phpp.sh
@@ -1,14 +1,13 @@
-sudo yum update -y
-cat /etc/system-release
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo systemctl is-enabled httpd
-curl httpd://localhost
-sudo systemctl is-enabled httpd
-curl http://localhost
-sudo usermod -a -G apache ec2-user
-exit
-
-cd /var/www/html/  
-echo "PHP Server 1" > index.html
+groups
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+sudo yum install php-mbstring php-xml -y
+sudo systemctl restart httpd
+sudo systemctl restart php-fpm
+cd /var/www/html/
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz
+sudo yum install wget
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz
+mkdir phpMyAdmin && tar -xvzf phpMyAdmin-latest-all-languages.tar.gz -C phpMyAdmin --strip-components 1
+rm phpMyAdmin-latest-all-languages.tar.gz
